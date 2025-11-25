065b0ce1952932ce55955e5e9eafd83e9e070e1f
JuaniSivo <juan_ignacio_sivori@yahoo.com.ar>
2025-11-25 18:21:05 +0100

    added data quality dimension validity/data ranges

--- my_project/data_quality.py
+++ my_project/data_quality.py
@@ -1,7 +1,8 @@
 import logging
+import logging.handlers
 import json
 from pathlib import Path
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Any
 
 import numpy as np
 import pandas as pd
@@ -12,17 +13,29 @@ P_EXT_DATA = P_DATA.joinpath("01_external")
 P_CONFIGS = Path("configs")
 P_DQR = P_CONFIGS.joinpath("data_quality_requirements.json")
 
+P_LOGS = Path("logs")
+P_LOG_FILENAME = P_LOGS.joinpath("data_auality.log")
+
 with open(P_DQR, "r") as f:
     dqr = json.load(f)
     
-PROD_DATA_TYPES = dqr.get("production")
-COMP_DATA_TYPES = dqr.get("completion")
+PROD_DQR = dqr.get("production")
+COMP_DQR = dqr.get("completion")
+
+DQR_TYPE = Dict[str, Dict[str, Any]] # {field: {required: true/false, "data_range": {"min":  2000}}}
 
 logger = logging.getLogger(__name__)
 
-def dtype_change(df: pd.DataFrame, dtype_dict: Dict[str, Dict[str, str]]) -> pd.DataFrame:
+class DataQualityRequirements():
+    pass
+
+class DataQualityAssessment():
+    pass
+
+def dtype_change(df: pd.DataFrame, dict_dqr: DQR_TYPE) -> pd.DataFrame:
+    logger.debug("Starting to convert data types")
     df_aux = df.copy(True)
-    for col, value in dtype_dict.items():
+    for col, value in dict_dqr.items():
         data_type = value["data_type"]
 
         # conversion to datetime
@@ -33,14 +46,18 @@ def dtype_change(df: pd.DataFrame, dtype_dict: Dict[str, Dict[str, str]]) -> pd.
                 errors="coerce",
                 format=date_format
             )
+            logger.debug(f"Converted {col} to datetime")
 
         # conversion to numbers
         if data_type.startswith(("float", "int")):
             df_aux[col] = pd.to_numeric(arg=df_aux[col], errors="coerce")
+            logger.debug(f"Converted {col} to numeric")
 
         # data type change in he DataFrame
         df_aux[col] = df_aux[col].astype(dtype=data_type, errors="raise")
+        logger.debug(f"Converted {col} to numeric")
 
+    logger.debug("All data types converted")
     return df_aux
 
 def calc_dataset_perc(df_bool: pd.DataFrame) -> np.float32:
@@ -59,19 +76,53 @@ def calc_column_null_count(df_bool: pd.DataFrame) -> pd.Series:
 
 def completness(df: pd.DataFrame) -> Tuple[np.float32, pd.Series, pd.Series]:
     completness = ~df.isnull()
+    logger.debug(f"Completeness: calculated mask for all columns")
     a = calc_dataset_perc(completness)
     b = calc_column_perc(completness)
     c = calc_column_null_count(completness)
+    logger.debug("COMPLETENESS metrics calculated")
 
     return (a, b, c)
 
-def validity(df: pd.DataFrame, dtype_dict: Dict[str, Dict[str, str]]) -> Tuple[np.float32, pd.Series, pd.Series]:
-    df_aux = dtype_change(df, dtype_dict)
-    validity = df_aux.isna() == df.isna()
+def valid_ranges_mask(df: pd.DataFrame, dict_dqr: DQR_TYPE) -> pd.DataFrame:
+    df_aux = df.copy(True)
+    df_validity = df == df
+    df_validity.loc[:, :] = True
+    for col, value in dict_dqr.items():
+        if "data_range" in value.keys():
+            data_range = value["data_range"]
+            missing = df_aux[col].isna()
+            if "min" in data_range.keys():
+                validity = df_aux[col] >= data_range["min"]
+                validity = validity | missing
+                df_validity.loc[:, col] = df_validity.loc[:, col] & validity
+                logger.debug(f"Validity range: calculated mask for {col}. Minimum: {data_range["min"]}")
+            if "max" in data_range.keys():
+                validity = df_aux[col] <= data_range["max"]
+                validity = validity | missing
+                df_validity.loc[:, col] = df_validity.loc[:, col] & validity
+                logger.debug(f"Validity range: calculated mask for {col}. Maximum: {data_range["max"]}")
+            if "set" in data_range.keys():
+                data_range_set = data_range["set"]
+                validity = df_aux[col].isin(data_range_set)
+                validity = validity | missing
+                df_validity.loc[:, col] = df_validity.loc[:, col] & validity
+                logger.debug(f"Validity range: calculated mask for {col}. Set with {len(data_range_set)} items")
+
+    return df_validity
+
+def validity(df: pd.DataFrame, dict_dqr: DQR_TYPE) -> Tuple[np.float32, pd.Series, pd.Series]:
+    df_aux = dtype_change(df, dict_dqr)
+    validity_type = df_aux.isna() == df.isna()
+    logger.debug(f"Validity data type: calculated mask for all columns")
+    validity_range = valid_ranges_mask(df_aux, dict_dqr)
+    logger.debug(f"Validity range: calculated mask for all columns")
+    validity = validity_type & validity_range
     a = calc_dataset_perc(validity)
     b = calc_column_perc(validity)
     c = calc_column_null_count(validity)
 
+    logger.debug("VALIDITY metrics calculated")
     return (a, b, c)
 
 def main():
@@ -79,14 +130,28 @@ def main():
     df_comp = pd.read_parquet(P_EXT_DATA.joinpath("completion.parquet"))
     
     print("-- COMPLETNESS --")
-    print(completness(df_prod))
+    completness(df_prod)
     
-    print("-- Validity --")
-    print(validity(df_prod, PROD_DATA_TYPES))
+    print("-- VALIDITY --")
+    validity(df_prod, PROD_DQR)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    # logging.basicConfig(level=logging.DEBUG)
 
-    logger.debug("Debugging module dataset")
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)12s - %(name)12s - %(levelname)12s - %(message)s')
+    
+    fh = logging.handlers.RotatingFileHandler(P_LOG_FILENAME, maxBytes=100000, backupCount=1)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    logger.info("Debugging module data quality")
     main()
-    logger.debug("Finished debugging module")
+    logger.info("Finished debugging module")
